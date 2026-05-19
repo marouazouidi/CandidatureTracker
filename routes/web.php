8e6fdefec1db8961979a37d86595bb8572bb974d
marouazouidi <zouidimaroua@gmail.com>
2026-05-19 11:49:04 +0100

    add Routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CandidatureController;
+use App\Http\Controllers\InterviewController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +17,32 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('/candidatures')->group(function(){
+
+        Route::controller(CandidatureController::class)->group(function(){
+            Route::get('/', 'index')->name('candidatures.index');
+            Route::get('/create', 'create')->name('candidatures.create');
+            Route::post('/', 'store')->name('candidatures.store');
+            Route::get('/archives', 'archives')->name('candidatures.archives');
+            Route::get('/{candidature}', 'show')->name('candidatures.show');
+            Route::get('/{candidature}/edit', 'edit')->name('candidatures.edit');
+            Route::put('/{candidature}', 'update')->name('candidatures.update');
+            Route::delete('/{candidature}', 'archive')->name('candidatures.archive');
+            Route::patch('/{candidature}/restore', 'restore')->name('candidatures.restore');
+            Route::delete('/{candidature}/force', 'forceDelete')->name('candidatures.forceDelete');
+        });
+
+        Route::controller(InterviewController::class)->group(function(){
+            Route::get('/{candidature}/interviews', 'index')->name('interviews.index');
+            Route::get('/{candidature}/interviews/create', 'create')->name('interviews.create');
+            Route::post('/{candidature}/interviews', 'store')->name('interviews.store');
+            Route::get('/{candidature}/interviews/{interview}', 'show')->name('interviews.show');
+            Route::get('/{candidature}/interviews/{interview}/edit', 'edit')->name('interviews.edit');
+            Route::put('/{candidature}/interviews/{interview}', 'update')->name('interviews.update');
+            Route::delete('/{candidature}/interviews/{interview}', 'destroy')->name('interviews.destroy');
+        });
+    });
 });
 
 require __DIR__.'/auth.php';
